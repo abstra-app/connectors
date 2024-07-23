3ba96cac357b85433d7f9b0871ad7526154bd80b
Bruno Vieira Costa <bruno@abstra.app>
2024-07-23 00:48:24 -0300

    import

--- connectors/airtable/__init__.py
+++ connectors/airtable/__init__.py
@@ -1,4 +1,14 @@
-from .methods import *
+from .methods import (
+    create_record,
+    create_records,
+    update_record,
+    update_records,
+    upsert_records,
+    delete_record,
+    delete_records,
+    get_record,
+    list_records,
+)
 
 __all__ = [
     "create_record",
